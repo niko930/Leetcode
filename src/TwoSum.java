ec5f32eb4f5e35f1f2e2362e8b29e755e6bf27a4
xndy930 <im.dengcm@gmail.com>
2019-03-25 15:24:26 +0800

    找出不含有重复字符的最长子串的长度

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -1,7 +1,7 @@
 import java.util.HashMap;
 import java.util.Map;
 
-public class Solution1 {
+public class TwoSum {
 
     public int[] twoSum(int[] nums, int target) {
         Map<Integer,Integer> map = new HashMap<>();
