f832b6557a71d024622ca7b0db98a059ed4dfce9
xndy930 <im.dengcm@gmail.com>
2019-03-25 15:27:40 +0800

    最长回文子串

--- src/LongestPalindrome.java
+++ src/LongestPalindrome.java
@@ -1,5 +1,15 @@
 public class LongestPalindrome {
-
+    /**
+     * 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+     *
+     * 示例 1：
+     *
+     * 输入: "babad"
+     * 输出: "bab"
+     * 注意: "aba" 也是一个有效答案。
+     * @param s
+     * @return
+     */
     private static String longestPalindrome(String s) {
         if(s == null || s.length() < 1) return "";
         int start = 0, end = 0;
