05c2ee33ae97ccac241d41e3772a0ab40ce6851f
xndy930 <im.dengcm@gmail.com>
2019-05-17 11:20:16 +0800

    保持城市天际线

--- src/MaxIncreaseKeepingSkyline.java
+++ src/MaxIncreaseKeepingSkyline.java
@@ -55,17 +55,6 @@ public class MaxIncreaseKeepingSkyline {
             }
         }
 
-
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[i].length;j++){
-                System.out.print(grid[i][j]);
-            }
-            System.out.println();
-        }
-
-
-
-
         return countNum;
     }
 
