5adc936a2a65945fbd13d5673b55d96e77dd2ee5
xndy930 <im.dengcm@gmail.com>
2019-05-17 11:55:54 +0800

    保持城市天际线

--- src/MaxIncreaseKeepingSkyline.java
+++ src/MaxIncreaseKeepingSkyline.java
@@ -1,5 +1,7 @@
 public class MaxIncreaseKeepingSkyline {
     /**
+     * 807. 保持城市天际线
+     *
      * 在二维数组grid中，grid[i][j]代表位于某处的建筑物的高度。 我们被允许增加任何数量（不同建筑物的数量可能不同）的建筑物的高度。 高度 0 也被认为是建筑物。
      *
      * 最后，从新数组的所有四个方向（即顶部，底部，左侧和右侧）观看的“天际线”必须与原始数组的天际线相同。 城市的天际线是从远处观看时，由所有建筑物形成的矩形的外部轮廓。 请看下面的例子。
@@ -31,27 +33,27 @@ public class MaxIncreaseKeepingSkyline {
      *  grid[i][j] 的高度范围是： [0, 100]。
      * 一座建筑物占据一个grid[i][j]：换言之，它们是 1 x 1 x grid[i][j] 的长方体。
      */
-    public int maxIncreaseKeepingSkyline(int[][] grid) {
+    private int maxIncreaseKeepingSkyline(int[][] grid) {
 
         int countNum = 0;
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[i].length;j++){
+        for (int[] ints : grid) {
+            for (int j = 0; j < ints.length; j++) {
                 //数组竖直方向
                 int index = 0;  //下标
                 int maxVertical = grid[index][j];
-                for(;index<grid.length-1;index++){
-                    maxVertical = Math.max(maxVertical, grid[index+1][j]);  //竖直方向最大值
+                for (; index < grid.length - 1; index++) {
+                    maxVertical = Math.max(maxVertical, grid[index + 1][j]);  //竖直方向最大值
                 }
 
                 //数组水平方向
                 index = 0;
-                int maxLevel = grid[i][index];
-                for(;index<grid[i].length-1;index++){
-                    maxLevel = Math.max(maxLevel, grid[i][index+1]);        //水平方向最大值
+                int maxLevel = ints[index];
+                for (; index < ints.length - 1; index++) {
+                    maxLevel = Math.max(maxLevel, ints[index + 1]);        //水平方向最大值
                 }
 
                 int remp = Math.min(maxVertical, maxLevel);     //grid[i][j]坐标竖直和水平方向相对小的值
-                countNum += remp - grid[i][j];      //记录总和
+                countNum += remp - ints[j];      //记录总和
             }
         }
 
