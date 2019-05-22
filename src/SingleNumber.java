718446c2eae8b3024841869a5dc32f1a4ac58802
xndy930 <im.dengcm@gmail.com>
2019-05-22 12:54:38 +0800

    136. 只出现一次的数字

--- src/SingleNumber.java
+++ src/SingleNumber.java
@@ -21,7 +21,7 @@ public class SingleNumber {
         if(nums.length < 1) return 0;
         int result = nums[0];
         for (int i = 1; i < nums.length; i++) {
-            result = result ^ nums[i];
+            result ^= nums[i];
         }
         return result;
     }
