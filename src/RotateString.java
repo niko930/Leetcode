ebc9e7438e250c2cf5a7b1b5923403b53839173f
xndy930 <im.dengcm@gmail.com>
2019-06-28 10:54:34 +0800

    旋转字符串

---
+++ src/RotateString.java
@@ -0,0 +1,39 @@
+public class RotateString {
+    /**
+     * 旋转字符串
+     *
+     * 给定一个字符串和一个偏移量，根据偏移量旋转字符串(从左向右旋转)
+     *
+     * 样例
+     *
+     * 对于字符串 "abcdefg".
+     *
+     * offset=0 => "abcdefg"
+     * offset=1 => "gabcdef"
+     * offset=2 => "fgabcde"
+     * offset=3 => "efgabcd"
+     */
+    public char[] rotateString(char[] A, int offset) {
+        if (A == null || A.length == 0) {
+            return A;
+        }
+
+        int len = A.length;
+        offset %= len;
+        reverse(A, 0, len - offset - 1);
+        reverse(A, len - offset, len - 1);
+        reverse(A, 0, len - 1);
+
+        return A;
+    }
+
+    private void reverse(char[] str, int start, int end) {
+        while (start < end) {
+            char temp = str[start];
+            str[start] = str[end];
+            str[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
