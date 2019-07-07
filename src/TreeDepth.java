01db7511fd67cde70ced6fc690602295434ff87b
xndy930 <im.dengcm@gmail.com>
2019-07-07 14:55:14 +0800

    二叉树的深度

---
+++ src/TreeDepth.java
@@ -0,0 +1,16 @@
+public class TreeDepth {
+    /**
+     * 二叉树的深度
+     *
+     * 输入一棵二叉树，求该树的深度。
+     * 从根结点到叶结点依次经过的结点（含根、叶结点）形成树的一条路径，最长路径的长度为树的深度。
+     */
+    public int TreeDepth(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        int nLeft = TreeDepth(root.left);
+        int nRight = TreeDepth(root.right);
+        return nLeft > nRight ? (nLeft+1) : (nRight+1);
+    }
+}
