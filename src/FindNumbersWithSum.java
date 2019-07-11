527c11a8da6fbc22b8d5be6b1712aa0d29fa4eea
xndy930 <im.dengcm@gmail.com>
2019-07-11 12:19:54 +0800

    和为S的两个数字

--- src/FindNumbersWithSum.java
+++ src/FindNumbersWithSum.java
@@ -11,30 +11,20 @@ public class FindNumbersWithSum {
      * 对应每个测试案例，输出两个数，小的先输出。
      */
     public ArrayList<Integer> FindNumbersWithSum(int [] array, int sum) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-
-        int[] result = new int[2];
-
-        for(int i=0;i<array.length;i++){
-            int temp = sum - array[i];
-            if(map.containsKey(temp)){
-                int res = result[0]*result[1];
-                if(res == 0){
-                    result[0] = array[i] < array[map.get(temp)] ? array[i] : array[map.get(temp)];
-                    result[1] = array[i] > array[map.get(temp)] ? array[i] : array[map.get(temp)];
-                }
-                if(res != 0 && res > array[i] * array[map.get(temp)]){
-                    result[0] = array[i] < array[map.get(temp)] ? array[i] : array[map.get(temp)];
-                    result[1] = array[i] > array[map.get(temp)] ? array[i] : array[map.get(temp)];
-                }
-            }else{
-                map.put(array[i], i);
-            }
-        }
         ArrayList<Integer> list = new ArrayList<>();
-        for(int i=0;i<result.length;i++){
-            if(result[i] != 0){
-                list.add(result[i]);
+        int i = 0, j = array.length - 1;
+        while(i < j){
+
+            if(array[i] + array[j] == sum){
+                list.add(array[i]);
+                list.add(array[j]);
+                break;
+            }
+            while(i < j && array[i] + array[j] > sum){
+                j--;
+            }
+            while(i < j && array[i] + array[j] < sum){
+                i++;
             }
         }
         return list;
