7f80df873416e5214a46fe19f2d41190a7279396
xndy930 <im.dengcm@gmail.com>
2019-08-13 14:17:54 +0800

    不用加减乘除做加法

--- src/Add.java
+++ src/Add.java
@@ -5,11 +5,11 @@ public class Add {
      * 写一个函数，求两个整数之和，要求在函数体内不得使用+、-、*、/四则运算符号。
      */
     public int Add(int num1, int num2) {
-        while (num2 != 0) {
-            int temp = num1 ^ num2;
-            num2 = (num1 & num2) << 1;
-            num1 = temp;
+        while(num1 != 0){
+            int remp = num1 ^ num2;
+            num1 = (num1&num2) << 1;
+            num2 = remp;
         }
-        return num1;
+        return num2;
     }
 }
