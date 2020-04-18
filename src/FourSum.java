12dee20afcfb34f0c9945b6d6d180f64796e7b5f
niko <im.dengcm@gmail.com>
2020-04-18 11:46:59 +0800

    四数之和

---
+++ src/FourSum.java
@@ -0,0 +1,71 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FourSum {
+
+    /**
+     *
+     * 18. 四数之和
+     *
+     * 给定一个包含 n 个整数的数组 nums 和一个目标值 target，判断 nums 中是否存在四个元素 a，b，c 和 d ，使得 a + b + c + d 的值与 target 相等？找出所有满足条件且不重复的四元组。
+     *
+     * 注意：
+     *
+     * 答案中不可以包含重复的四元组。
+     *
+     * 示例：
+     *
+     * 给定数组 nums = [1, 0, -1, 0, -2, 2]，和 target = 0。
+     *
+     * 满足要求的四元组集合为：
+     * [
+     *   [-1,  0, 0, 1],
+     *   [-2, -1, 1, 2],
+     *   [-2,  0, 0, 2]
+     * ]
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/4sum
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        int len = nums.length;
+        if (nums == null || len < 4) {
+            return result;
+        }
+        for (int i = 0; i < len - 3; i++) {
+            if (i > 0 && nums[i - 1] == nums[i]) {
+                continue;
+            }
+            for (int j = i + 1; j < len - 2; j++) {
+                if (j > i + 1 && nums[j - 1] == nums[j]) {
+                    continue;
+                }
+                int L = j + 1, R = len - 1;
+                while (L < R) {
+                    int sum = nums[i] + nums[j] + nums[L] + nums[R];
+                    if (sum == target) {
+                        result.add(Arrays.asList(nums[i], nums[j], nums[L], nums[R]));
+                        while (L < R && nums[L] == nums[L+1]) L++;
+                        while (L < R && nums[R] == nums[R - 1]) R--;
+                        L++;
+                        R--;
+                    } else if (sum < target) {
+                        L++;
+                    } else {
+                        R--;
+                    }
+                }
+            }
+
+        }
+        return result;
+    }
+}
